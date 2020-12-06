fa51affef5f55c2d8b2c3d407605d3a2c202b61c
tezoro <cocainovych@gmail.com>
2020-12-06 23:40:09 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
+import Music from './components/Music/Music';
 import Navbar from './components/Navbar/navBar';
+import News from './components/News/News';
 import Profile from './components/Profile/Profile';
+import Settings from './components/Settings/Settings';
 
 function App() {
   return (
+    <BrowserRouter>
       <div className="app-wrapper">
           <Header />
           <Navbar />
         <div className='app-wrapper-content'>
-          <Dialogs />
-        </div>
-          {/*<Profile />*/}
+        <Route path="/Dialogs" component={Dialogs}/>
+        <Route path="/Profile"component={Profile}/>
+        <Route path="/News" component={News} />
+        <Route path="/Music" component={Music} />
+        <Route path="/Settings" component={Settings} />
+        </div> 
       </div>
+    </BrowserRouter>
       )
   }
 
--- src/components/Navbar/navBar.jsx
+++ src/components/Navbar/navBar.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from "./Navbar.module.css";
 
 function Navbar(){
    return(
 <nav className={s.nav}>
-  <div className={`${s.item} ${s.active}`}><a href="/Profile">Profile</a></div>
-  <div className={s.item}><a href="/Dialogs">Messages</a></div>
-  <div className={s.item}><a href="">Blog</a></div>
-  <div className={s.item}><a href="">Settings</a></div>
+  <div className={s.item}>
+      <NavLink to="/Profile" activeClassName={s.activeLink}>Profile</NavLink>
+     </div>
+  <div className={s.item}>
+     <NavLink to="/Dialogs" activeClassName={s.activeLink}>Messages</NavLink ></div>
+  <div className={s.item}>
+      <NavLink to="/News" activeClassName={s.activeLink}>News</NavLink >
+  </div>
+  <div className={s.item}>
+            <NavLink to="/Music" activeClassName={s.activeLink}>Music</NavLink >
+     </div>
+  <div className={s.item}>
+            <NavLink to="Settings" activeClassName={s.activeLink}>Settings</NavLink >
+     </div>
 </nav>
    )
 }
 
-export default Navbar;
+export default Navbar
